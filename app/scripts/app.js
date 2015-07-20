4d51b5841511286142356e81747ec5693359b648
hookemtweets <hookemtweets@gmail.com>
2015-07-20 20:56:16 +0000

    Angular Repo

---
+++ app/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name workspaceApp
+ * @description
+ * # workspaceApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('workspaceApp', [
+    'ngCookies',
+    'ngTouch'
+  ]);
